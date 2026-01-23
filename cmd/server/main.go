e57e2038cef9973b83054b045de8e5a66d697496
Amro Al Deek <amrobasheer242@gmail.com>
2026-01-24 00:17:54 +0200

    check the server after stopping the ec2 instance

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,6 +7,7 @@ import (
 func main() {
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Dealna CI/CD third deploy"))
+		w.Write([]byte("Al hamdulillah"))
 	})
 
 	http.ListenAndServe(":8080", nil)
