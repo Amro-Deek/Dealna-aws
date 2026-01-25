04f921e217f92efcf857e4a79d3dc0c0d125847b
Amro Al Deek <amrobasheer242@gmail.com>
2026-01-25 16:03:32 +0200

    postgress connection added

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,44 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
 )
 
+func connectDB() *sql.DB {
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("✅ DATABASE CONNECTED")
+	return db
+}
+
 func main() {
+	db := connectDB()
+
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Dealna CI/CD fourth deploy"))
-		
+		w.Write([]byte("Dealna backend running"))
 	})
 
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
