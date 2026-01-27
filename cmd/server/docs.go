f8ae77616ea43e206ebcf83ab2a97bd1ada99c70
Amro Al Deek <amrobasheer242@gmail.com>
2026-01-28 01:13:37 +0200

    fixing host number for deployment so tests success

--- cmd/server/docs.go
+++ cmd/server/docs.go
@@ -1,13 +1,13 @@
 package main
 
-//	@title			Dealna API
-//	@version		1.0
-//	@description	Dealna backend API (Authentication & User Profile)
-//	@host			localhost:8080
-//	@BasePath		/api/v1
+// @title           Dealna API
+// @version         1.0
+// @description     Dealna backend API (Authentication & User Profile)
+// @host 			98.92.82.224:8080
+// @BasePath        /api/v1
 
-//	@securityDefinitions.apikey	BearerAuth
-//	@in							header
-//	@name						Authorization
+// @securityDefinitions.apikey BearerAuth
+// @in              header
+// @name            Authorization
 
 func SwaggerInfo() {}
