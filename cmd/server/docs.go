27e8f2a6c891b1c54b5007f348eaeb2b932c61c3
Amro Al Deek <amrobasheer242@gmail.com>
2026-01-28 01:21:47 +0200

    fixing server issue

--- cmd/server/docs.go
+++ cmd/server/docs.go
@@ -3,7 +3,7 @@ package main
 // @title           Dealna API
 // @version         1.0
 // @description     Dealna backend API (Authentication & User Profile)
-// @host 			98.92.82.224:8080
+// @server          http://98.92.82.224:8080/api/v1
 // @BasePath        /api/v1
 
 // @securityDefinitions.apikey BearerAuth
